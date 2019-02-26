f6aa111c0893dd21c2a11a283c0a1e210713d776
asitkdash <asitdash.csu.fullerton.edu>
2019-02-25 19:22:49 -0800

    callback function implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
 import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
   constructor(props) {
@@ -19,6 +20,10 @@ class App extends Component {
             path="/"
             exact
             render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
           />
           <Route path="/profile" component={Profile} />
         </div>

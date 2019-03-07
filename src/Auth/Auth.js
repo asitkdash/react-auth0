e8d071fce11d1fe5f1e0d784ff23a6a44fe51001
asitkdash <asitdash.csu.fullerton.edu>
2019-03-07 01:36:29 -0800

    profile page set up

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,6 +3,7 @@ import auth0 from "auth0-js";
 export default class Auth {
   constructor(history) {
     this.history = history;
+    this.userProfile = null;
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -48,10 +49,26 @@ export default class Auth {
     localStorage.removeItem("access_token");
     localStorage.removeItem("id_token");
     localStorage.removeItem("expires_at");
+    this.userProfile = null;
     //    this.history.push("/");
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       returnTo: "http://localhost:3000"
+    });
+  };
+  getAccessToken = () => {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+      throw new Error("No access token found.");
+    }
+    return accessToken;
+  };
+
+  getProfile = cb => {
+    if (this.userProfile) return cb(this.userProfile);
+    this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+      if (profile) this.userProfile = profile;
+      cb(profile, err);
     });
   };
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Profile extends Component {
+  state = {
+    profile: null,
+    error: ""
+  };
+
+  componentDidMount() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.props.auth.getProfile((profile, error) =>
+      this.setState({ profile, error })
+    );
+  }
+
   render() {
+    const { profile } = this.state;
+    if (!profile) return null;
     return (
-      <div>
+      <>
         <h1>Profile</h1>
-      </div>
-    )
+        <p>{profile.nickname}</p>
+        <img
+          style={{ maxWidth: 50, maxHeight: 50 }}
+          src={profile.picture}
+          alt="profile pic"
+        />
+        <pre>{JSON.stringify(profile, null, 2)}</pre>
+      </>
+    );
   }
 }
 
